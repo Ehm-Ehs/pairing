d50fbf2150a5989587498a0423c689a05bb92e07
Ehm-Ehs <salamimariam239@gmail.com>
2025-12-04 00:28:14 +0100

    fix: add .env

--- src/components/api/firebase.tsx
+++ src/components/api/firebase.tsx
@@ -8,18 +8,17 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCx3DQLMhR6tCGZh6FHKTcqcSKxf5NFS-U",
-  authDomain: "pairing-login.firebaseapp.com",
-  projectId: "pairing-login",
-  storageBucket: "pairing-login.appspot.com",
-  messagingSenderId: "903704917637",
-  appId: "1:903704917637:web:13cdadbee90deb0cc95bec",
-  measurementId: "G-BBZJ01DP7V",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth();
 export const db = getFirestore(app);
